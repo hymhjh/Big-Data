f10c809abf415d1c14d286c992c24f497ac7c901
Yiman Huang <huangyiman1@gmail.com>
2017-02-12 20:35:28 -0500

    Create Counter.java

---
+++ bike/biketimeweek/Counter.java
@@ -0,0 +1,35 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class Counter{
+    
+    public static void main(String[] args) throws Exception {
+        
+        if(args.length != 2){
+            System.err.println("Usage : Counter <input path 1> <output path>");
+            System.exit(-1);
+        }
+        
+        
+        Job job = new Job();
+        job.setJarByClass(Counter.class);
+        job.setJobName("Counter");
+        
+        job.setMapperClass(CountMapper.class);
+        job.setReducerClass(CountReducer.class);
+        
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setNumReduceTasks(1);
+        
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
